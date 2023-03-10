7b720f85ed8b6d07cc8fc214a233025a04991eed
sebuszqo <sekn.kn@gmail.com>
2023-03-10 18:18:30 +0100

    Added few solutions to select homework tasks

--- databases/homework-select-northwind.sql
+++ databases/homework-select-northwind.sql
@@ -4,7 +4,7 @@ use Northwind
 -- Column selection exercises
 
 -- 1. Select names and addresses of all customers.
-select CompanyName, Address
+select CompanyName, Address, City
 from Customers;
 
 -- 2. Select surnames and phone numbers of employees.
@@ -78,9 +78,11 @@ from Products where QuantityPerUnit like '%bottle%'
 select FirstName, Title
 from Employees where LastName like '[B-L]%'
 
+select Title, LastName from Employees where LEFT(LastName, 1) between 'b' and 'l'
+
 -- 3. Retrieve information about the job titles of employees whose last names start with the letter B or L.
 select FirstName, Title
-from Employees where LastName like '[B,L]%'
+from Employees where LastName like '[BL]%'
 
 -- 4. Find the names of categories that contain a comma in their description.
 select CategoryName
@@ -122,6 +124,8 @@ select CompanyName, Country from Customers where Country in ('Japan', 'Italy')
 -- Write a SELECT statement to retrieve the order number, order date, and customer number
 -- for all unfulfilled orders where the recipient's country is Argentina.
 select OrderID, OrderDate, CustomerID from Orders where (ShipCountry ='Argentina' and ShippedDate is NULL)
+select getdate()
+
 
 
 -- ORDER BY exercises
@@ -170,3 +174,79 @@ from [Order Details] where OrderID=10250
 
 select Fax + ',' + Phone as 'fax and phone'
 from Suppliers
+
+select Fax + ',' + Phone as 'fax and phone'
+from Suppliers where Fax is not null and Phone is not null
+
+-- Napisz polecenie które dla każdego dostawcy (supplier) pokaże pojedynczą kolumnę zawierającą nr telefonu i nr faksu w formacie
+-- (numer telefonu i faksu mają być oddzielone przecinkiem)
+select CompanyName, concat(Fax, ',', Phone) as 'Fax + Phone' from Suppliers
+
+
+SELECT IIF(Fax IS NULL, Phone, CONCAT(Phone, ',', Fax)) AS formatted FROM Suppliers
+
+-- isnull change first parameter to the second one when the first one is null
+select isnull(null, 9) from Suppliers
+
+SELECT CONCAT(Phone, ISNULL(',' + Fax, '')) AS formatted FROM Suppliers
+
+
+-- Homework
+
+-- pokaż zamówienia złożone w marcu 97
+select OrderDate
+from Orders where DATEPART(year,OrderDate) = '1997' and DATEPART(month, OrderDate) = '3'
+
+select OrderDate
+from Orders where MONTH(OrderDate) = 3 and YEAR(OrderDate) = 1997;
+
+SELECT OrderDate,* FROM Orders WHERE OrderDate BETWEEN '1997-03-01' AND '1997-03-31';
+
+-- Dla każdego zamówienia wyświetl ile dni upłynęło od daty zamówienia do daty wysyłki
+SELECT DATEDIFF(day,OrderDate, ShippedDate), OrderDate, ShippedDate AS dni_do_wysylki
+FROM Orders
+
+-- Pokaz przeterminowane zamówienia
+select RequiredDate, ShippedDate
+from Orders WHERE ShippedDate > RequiredDate
+
+-- How many years has each employee worked for the company?
+-- For each employee, display their first name, last name, and age.
+select FirstName,LastName, DATEDIFF(year ,HireDate, GETDATE()) as 'years of being hired' from Employees
+
+-- Display all employees who are now over 25 years old.
+select *, DATEDIFF(year, BirthDate, GETDATE()) as year from Employees where DATEDIFF(year, BirthDate, GETDATE()) > 25
+
+SELECT FirstName, LastName, BirthDate,
+DATEDIFF(YEAR, BirthDate, GETDATE()) AS age
+FROM employees
+WHERE DATEADD(YEAR, 25, BirthDate) <= GETDATE()
+
+-- Calculate the average number of months each employee has worked for the company.
+SELECT AVG(DATEDIFF(MONTH, HireDate, GETDATE())) AS avg_months_worked
+FROM employees
+
+-- Display data for all employees who have worked for the company for at least 320 months but no more than 333 months.
+select * from Employees where DATEDIFF(month, HireDate, GETDATE()) between 320 and 333
+
+-- For each employee, display their first name, last name, year of birth, year in which they were hired, and the number of years they were when hired.
+SELECT FirstName, LastName, YEAR(BirthDate) AS year_of_birth,
+YEAR(HireDate) AS year_of_hire, DATEDIFF(YEAR, BirthDate, HireDate) AS age_when_hired
+FROM employees;
+
+
+-- For each employee, display their first name, last name, date of birth (year, month, and day), and the day of the week they were born.
+SELECT FirstName, LastName,
+YEAR(BirthDate) AS birth_year, MONTH(BirthDate) AS birth_month, DAY(BirthDate) AS birth_day,
+DATENAME(WEEKDAY, BirthDate) AS birth_weekday
+FROM employees;
+
+-- Count how many letters the shortest employee name consists of.
+SELECT MIN(LEN(FirstName)) as shortest_firstName
+FROM employees
+
+-- Display how many years have passed since January 1, 1990.
+
+select DATEDIFF(year, '1990-01-01', GETDATE())
+
+-- select GETDATE() as CurrentDate;
