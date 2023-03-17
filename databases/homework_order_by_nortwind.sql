60be5ef872e94b964a74e35f787af260099048e2
sebuszqo <sekn.kn@gmail.com>
2023-03-17 20:19:28 +0100

    The end of second list of tasks about group by and having, etc...

--- databases/homework_order_by_nortwind.sql
+++ databases/homework_order_by_nortwind.sql
@@ -1,3 +1,5 @@
+-- TASKS --> order by, having, min, max, count, sum, avg, etc...
+
 -- difference between counting and no counting nulls
 USE northwind
 SELECT COUNT(*)
@@ -107,6 +109,83 @@ order by sum(Freight) desc
 -- having count(Orders.CustomerID) > 8
 -- order by CustomerID
 
+
+
+-- 2_1 group by tasks
+
+-- Write a command that calculates the sales value for each order in the order details table and returns the result sorted in descending order (by sales value).
+select OrderID, sum(Quantity * UnitPrice *(1-Discount)) as value
+from [Order Details]
+group by OrderID
+order by value desc
+
+-- Modify the query from the previous point to return the first 10 rows.
+select top 10 with ties OrderID, sum(Quantity * UnitPrice *(1-Discount)) as value
+from [Order Details]
+group by OrderID
+order by value desc
+
+-- Provide the number of ordered units for products where productid < 3.
+select  ProductID, sum(Quantity)
+from [Order Details]
+where ProductID < 3
+group by ProductID
+
+-- Modify the query from the previous point to provide the number of ordered units for all products.
+select  ProductID, sum(Quantity)
+from [Order Details]
+group by ProductID
+
+-- Provide the order number and order value for orders where the total number of ordered units for products is > 250.
+select OrderID, sum(Quantity * UnitPrice) as OrderValue
+from [Order Details]
+group by OrderID
+having sum(Quantity) > 250
+
+-- select * from [Order Details] where OrderID=10515
+
+-- For each employee, give the number of orders they handled:
+
+select FirstName, Orders.EmployeeID ,count(*)
+from orders
+inner join Employees E on Orders.EmployeeID = E.EmployeeID
+group by Orders.EmployeeID, FirstName
+
+-- For each shipper/carrier, give the value of the "shipping fee" for the orders they transported:
+
+select ShipVia, CompanyName, sum(Freight) as shippingFees
+from Orders
+inner join Shippers s on Orders.ShipVia = s.ShipperID
+group by ShipVia, CompanyName
+
+-- For each shipper/carrier, give the value of the "shipping fee" for the orders they transported in the years 1996 to 1997:
+select ShipVia, CompanyName, sum(Freight) as shippingFees
+from Orders
+inner join Shippers s on Orders.ShipVia = s.ShipperID
+    where year(ShippedDate) between 1996 and 1997
+group by ShipVia, CompanyName
+
+-- For each employee, give the number of orders they handled, broken down by year and month:
+SELECT
+    EmployeeID,
+    YEAR(OrderDate) AS OrderYear,
+    MONTH(OrderDate) AS OrderMonth,
+    COUNT(*) AS OrdersHandled
+FROM Orders
+GROUP BY EmployeeID, YEAR(OrderDate), MONTH(OrderDate)
+
+-- For each category, give the maximum and minimum price of the products in that category:
+select  Products.CategoryID, Categories.CategoryName, max(UnitPrice) as max, min(UnitPrice) as min
+from Products
+inner join Categories on Products.CategoryID = Categories.CategoryID
+group by Products.CategoryID, Categories.CategoryName
+
+SELECT
+    CategoryID,
+    MAX(UnitPrice) AS MaxPrice,
+    MIN(UnitPrice) AS MinPrice
+FROM Products
+GROUP BY CategoryID
 
 
 
